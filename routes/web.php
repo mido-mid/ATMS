7791752fa16f0327d987e6ed98dc0443a5399fed
m.osama <m.osama@eramint.com>
2020-11-13 13:44:16 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+Route::get('/logout',function(){
+
+    if(\Auth::check())
+    {
+        \Auth::logout();
+        return redirect('/home');
+    }
+    else
+    {
+        return redirect('/');
+    }
+})->name('logout');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin','middleware' => ['auth','admin']],function() {
+    Route::resource('admins', 'Admin\AdminController', ['except' => ['show']]);
+});
+
+Route::group(['prefix' => 'head','middleware' => ['auth','head']],function() {
+
+    Route::resource('categories', 'Admin\CategoryController', ['except' => ['show']]);
+});
+
+Route::group(['prefix' => 'employee','middleware' => ['auth','employee','verified']],function() {
+
+    Route::resource('admins', 'Admin\AdminController', ['except' => ['show']]);
 });
